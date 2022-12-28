13c6d2fa2cb869ebe3f0a5f936291f7e360aa928
wjdgh4058 <ljhmoa@gmail.com>
2022-12-28 10:50:38 +0900

    [추가] variables 학습

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,37 @@
+/* --------------------------------------------------------------------- */
+/* Variables                                                             */
+/* --------------------------------------------------------------------- */
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+console.log(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = "jungho";
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductQuantity = "313541351";
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+// - 구매 결제 여부
+let isPayment = "true";
+// - 구매 결제 내역
+let paymentHistory = "기타 등등";
+// - 브랜드 접두사
+const BRAND_PREFIX = "apple";
+// - 오늘의 운세
+let fortuneOfToday = "좋음";
+// - 상품 정보
+let productInfo = "사과";
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
