d39fa081ef1d223b3865825bfc7b95fa299bbfaf
wjdgh4058 <ljhmoa@gmail.com>
2023-01-11 22:22:53 +0900

    [추가]bindEvent 함수 만들기

---
+++ client/chapter/dom/05.styling.js
@@ -0,0 +1,25 @@
+/* ---------------------------------------------------------------------- */
+/* DOM Styling                                                            */
+/* ---------------------------------------------------------------------- */
+
+/* 클래스를 관리할 수 있게 해주는 DOM 프로퍼티 ------------------------------------ */
+let first = getNode(".first");
+// - className – 클래스 전체를 문자열 형태로 반환해주는 프로퍼티로 클래스 전체를 관리할 때 유용
+// - classList – 클래스 하나를 관리할 수 있게 해주는 메서드로 개별 클래스를 조작할 때 유용
+
+// console.log((first.className = "hola"));
+// first.classList.add("hello");
+// first.classList.remove("hello");
+// console.log(first.classList.contains("hello"));
+/* 스타일 변경 방법 --------------------------------------------------------- */
+
+// - style.cssText - "style" 속성 전체에 대응하므로 스타일 전체에 대한 문자열 저장
+first.style.transform = "rotate(360deg";
+
+let size = getComputedStyle(first).fontSize;
+// console.log(size);
+/* 계산된 스타일 읽기 ------------------------------------------------------- */
+
+// - getComputedStyle(element, [pseudoElement]) `읽기 전용`
+
+console.log(css(".first", "font-size"));
