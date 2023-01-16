1c7f5d67c09286c8753138a6cd68d0a0fa32b6a3
wjdgh4058 <ljhmoa@gmail.com>
2023-01-16 11:00:16 +0900

    [추가] 주사위 굴리기 이벤트 추가

--- client/main.js
+++ client/main.js
@@ -1,1 +1,11 @@
-import { diceAnimation } from "./lib/index.js";
+import { diceAnimation, getNode } from "./lib/index.js";
+
+const rollingDiceButton = getNode(".buttonGroup > button:nth-child(1)");
+
+let stopAnimation;
+
+const handlerRollingDice = () => {
+  stopAnimation = setInterval(diceAnimation, 100);
+};
+
+rollingDiceButton.addEventListener("click", handlerRollingDice);
