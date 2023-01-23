09595a391da1da5287e3a4d63fde1de38905af6e
wjdgh4058 <ljhmoa@gmail.com>
2023-01-23 17:52:00 +0900

    [추가] storage

--- client/main.js
+++ client/main.js
@@ -1,1 +1,34 @@
-console.log(window);
+import {
+  deleteStorage,
+  getNode,
+  loadStorage,
+  saveStorage,
+} from "./lib/index.js";
+
+const textField = getNode("#textField");
+const deleteButton = getNode('input[value="삭제"]');
+const sendButton = getNode('input[value="전송"]');
+
+loadStorage("area").then((res) => {
+  textField.value = res;
+});
+
+function inputHandler() {
+  saveStorage("area", textField.value);
+}
+
+textField.addEventListener("input", inputHandler);
+
+function deleteHandler() {
+  deleteStorage("area");
+}
+
+deleteButton.addEventListener("click", deleteHandler);
+
+/* function sendHandler() {
+  saveStorage("area", textField.value).then(() => {
+    textField.value = "";
+  });
+}
+
+sendButton.addEventListener("click", sendHandler); */
